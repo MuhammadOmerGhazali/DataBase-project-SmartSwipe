a4250a77749d4e9cb12497a1de89f95510b59cfe
abubakarmunir712 <ABUBAKARMUNIR712@GMAIL.COM>
2024-04-28 01:36:36 +0500

    Completed product api

--- Backend/app.js
+++ Backend/app.js
@@ -6,18 +6,30 @@ const productRoutes = require('./routes/products');
 const app = express();
 
 
-//Global middleware
 
+
+// Middleware to parse URL-encoded bodies
+app.use(bodyParser.urlencoded({ extended:false}));
+
+//Converting body to json format
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    console.log(req.path,req.method)
-    next();
-})
+//Error handling for invalid json data
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+        res.status(400).send('Invalid JSON data');
+    } else {
+        next(err); // Pass the error to the next middleware
+    }
+});
+
+
+
 
 app.use('/api/products',productRoutes);
 
 //listening to port
 app.listen(process.env.PORT,() =>{
     console.log('listening on port', process.env.PORT);
+    
 });
--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -1,37 +1,120 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../databaseConnection');
 
 
 
 //Get all products
 router.get('/', (req, res) => {
-    res.send('Get all products');
+    pool.getConnection((err, connetion) => {
+        if (err){
+            connetion.release();
+            return res.status(500).send('Internal Server Error');
+        }
+        connetion.query('Select * from products', (err, products) => {
+            connetion.release();
+
+            if (!err) {
+                res.send(products)
+            }
+            else {
+                return res.status(500).send(err.message);
+            }
+        })
+
+    })
 });
 
 
 //Get single product
 router.get('/:id', (req, res) => {
-    res.send('Get single Product');
- });
+    pool.getConnection((err, connetion) => {
+        if (err){
+            connetion.release();
+            return res.status(500).send('Internal Server Error');
+        }
+        connetion.query('Select * from products where ProductID = ?', [req.params.id], (err, products) => {
+            connetion.release();
+
+            if (!err) {
+                res.send(products)
+            }
+            else {
+                return res.status(500).send(err.message);
+            }
+        })
+
+    })
+});
 
 
- //Post a product
-router.post('/', (req, res)=>{
-    res.send('Post a product');
+//Post a product
+router.post('/', (req, res) => {
+
+
     
+    pool.getConnection((err,connetion) =>{
+        if (err){
+            connetion.release();
+            return res.status(500).send('Internal Server Error');
+        }
+        connetion.query('Insert into products SET ? ',[req.body],(err,product) =>{
+            if(!err){
+                res.send("Inserted successfully!")
+
+            }
+            else{
+                return res.status(500).send(err.message);
+            }
+        })
+    })
+
 });
 
 
 //Delete a product
-router.delete('/:id', (req, res)=>{
-    res.send('Delete a product');
-    
+router.delete('/:id', (req, res) => {
+    pool.getConnection((err, connetion) => {
+        if (err){
+            connetion.release();
+            return res.status(500).send('Internal Server Error');
+        }
+        connetion.query('Delete from products where ProductID = ?', [req.params.id], (err, products) => {
+            connetion.release();
+
+            if (!err) {
+                res.send("Deleted successfully!")
+            }
+            else {
+                return res.status(500).send(err.message);
+            }
+        })
+
+    })
 });
 
 
 //Update a product
-router.patch('/:id', (req, res)=>{
-    res.send('Update a product');
+router.patch('/:id', (req, res) => {
     
+    
+
+    pool.getConnection((err, connection) => {
+        if (err) {
+            connection.release();
+            return res.status(500).send('Internal Server Error');
+        }
+        
+        connection.query('UPDATE products SET ? WHERE ProductID = ?', [req.body, req.params.id], (err, result) => {
+            connection.release();
+            if (!err) {
+                res.send("Updated successfully!")
+            }
+            else {
+                return res.status(500).send(err.message);
+            }
+        });
+    });
 });
-module.exports = router;
+
+    module.exports = router;
